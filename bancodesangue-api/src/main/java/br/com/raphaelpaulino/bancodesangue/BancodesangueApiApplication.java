22c399c5292b62cdb6f1be26eae2595734436d13
Raphael Paulino <raphael.paulino.web@gmail.com>
2025-06-07 00:03:29 -0300

    inicia o projeto com Spring Boot

---
+++ bancodesangue-api/src/main/java/br/com/raphaelpaulino/bancodesangue/BancodesangueApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.raphaelpaulino.bancodesangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodesangueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodesangueApiApplication.class, args);
+	}
+
+}
---
+++ bancodesangue-api/src/test/java/br/com/raphaelpaulino/bancodesangue/BancodesangueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphaelpaulino.bancodesangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodesangueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
