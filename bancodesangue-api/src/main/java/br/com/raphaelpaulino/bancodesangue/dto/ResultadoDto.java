c6740acb1037bc38025d4c9bbc4f2aeff30dde0c
Raphael Paulino <raphael.paulino.web@gmail.com>
2025-06-11 03:15:26 -0300

    feat: cria o método imcPorFaixa() na camada de servico e aplica na classe ResultadoDto

--- bancodesangue-api/src/main/java/br/com/raphaelpaulino/bancodesangue/dto/ResultadoDto.java
+++ bancodesangue-api/src/main/java/br/com/raphaelpaulino/bancodesangue/dto/ResultadoDto.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class ResultadoDto {
 	private Map<String, Long> candidatosPorEstado;
+	
+	private Map<String, Double> imcMedioPorFaixaEtaria;
 
 	public Map<String, Long> getCandidatosPorEstado() {
 		return candidatosPorEstado;
@@ -11,5 +13,13 @@ public class ResultadoDto {
 
 	public void setCandidatosPorEstado(Map<String, Long> candidatosPorEstado) {
 		this.candidatosPorEstado = candidatosPorEstado;
+	}
+	
+	public Map<String, Double> getImcMedioPorFaixaEtaria() {
+		return imcMedioPorFaixaEtaria;
+	}
+	
+	public void setImcMedioPorFaixaEtaria(Map<String, Double> imcMedioPorFaixaEtaria) {
+		this.imcMedioPorFaixaEtaria = imcMedioPorFaixaEtaria;
 	}
 }
--- bancodesangue-api/src/main/java/br/com/raphaelpaulino/bancodesangue/service/CandidatoService.java
+++ bancodesangue-api/src/main/java/br/com/raphaelpaulino/bancodesangue/service/CandidatoService.java
@@ -1,25 +1,62 @@
 package br.com.raphaelpaulino.bancodesangue.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import br.com.raphaelpaulino.bancodesangue.domain.model.Candidato;
+import br.com.raphaelpaulino.bancodesangue.dto.CandidatoDto;
 import br.com.raphaelpaulino.bancodesangue.dto.ResultadoDto;
 
 @Service
 public class CandidatoService {
 
-	public ResultadoDto processar(List<Candidato> candidatos) {
+	public ResultadoDto processar(List<CandidatoDto> candidatos) {
 		ResultadoDto resultado = new ResultadoDto();
 		resultado.setCandidatosPorEstado(contarPorEstado(candidatos));
+		resultado.setImcMedioPorFaixaEtaria(imcPorFaixa(candidatos));
+		
 		return resultado;
 	}
 
-	private Map<String, Long> contarPorEstado(List<Candidato> candidatos) {
+	private Map<String, Long> contarPorEstado(List<CandidatoDto> candidatos) {
 		return candidatos.stream()
-				.collect(Collectors.groupingBy(Candidato::getEstado, Collectors.counting()));
+				.collect(Collectors.groupingBy(CandidatoDto::getEstado, Collectors.counting()));
+	}
+	
+
+	private Map<String, Double> imcPorFaixa(List<CandidatoDto> candidatos) {
+		Map<String, List<Double>> imcsPorFaixa = new HashMap<>();
+
+		for (CandidatoDto c : candidatos) {			
+			int idade = Period.between(c.getDataNasc(), LocalDate.now()).getYears();
+			String faixa = ((idade / 10) * 10) + "-" + (((idade / 10) * 10) + 9);
+
+			double imc = c.getPeso() / (c.getAltura() * c.getAltura());
+			imcsPorFaixa
+				.computeIfAbsent(faixa, k -> new ArrayList<>())
+				.add(imc);
+		}
+
+		Map<String, Double> medias = new TreeMap<>();
+		for (Map.Entry<String, List<Double>> e : imcsPorFaixa.entrySet()) {
+
+	        double media = e.getValue()
+				.stream()
+				.mapToDouble(Double::doubleValue)
+				.average()
+				.orElse(0.0);
+
+	            medias.put(e.getKey(), Math.round(media * 100.0) / 100.0); // arredondar para 2 casas
+		}
+		return medias;
 	}
 }
