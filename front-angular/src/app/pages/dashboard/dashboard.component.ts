985dc6fd5fdcc6baa35bc2bd494f7f7c6e449f4f
Raphael Paulino <raphael.paulino.web@gmail.com>
2025-06-12 04:52:47 -0300

    feat: cria o candidatos.service angular e faz diversos ajustes de layout

--- front-angular/src/app/pages/dashboard/dashboard.component.ts
+++ front-angular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import {
   ApexAxisChartSeries,
@@ -6,41 +6,71 @@ import {
   ApexXAxis,
   ApexTitleSubtitle,
   ApexDataLabels,
+  ApexPlotOptions,
+  ApexResponsive,
+  ApexLegend,
+  ApexFill,
+  ApexStroke,
+  ApexTooltip,
 } from 'ng-apexcharts';
+import { CandidatosService } from '../../services/candidatos.service';
+import { CommonModule } from '@angular/common';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+  plotOptions?: ApexPlotOptions;
+  dataLabels?: ApexDataLabels;
+  responsive?: ApexResponsive[];
+  legend?: ApexLegend;
+  fill?: ApexFill;
+  stroke?: ApexStroke;
+  tooltip?: ApexTooltip;
+};
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgApexchartsModule],
+  imports: [NgApexchartsModule, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss',
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  chartOptions: {
-    series: ApexAxisChartSeries;
-    chart: ApexChart;
-    xaxis: ApexXAxis;
-    title: ApexTitleSubtitle;
-    dataLabels: ApexDataLabels;
-  } = {
-    series: [
-      {
-        name: 'Média IMC',
-        data: [21.4, 22.5, 23.7, 25.3],
-      },
-    ],
-    chart: {
-      type: 'bar',
-      height: 350,
-    },
-    title: {
-      text: 'IMC por Faixa Etária',
-    },
-    xaxis: {
-      categories: ['16-19', '20-29', '30-39', '40-49'],
-    },
-    dataLabels: {
-      enabled: false,
-    },
-  };
+export class DashboardComponent implements OnInit {
+  public chartOptionsImc: Partial<ChartOptions> = {};
+
+  constructor(private candidatosService: CandidatosService) {}
+
+  ngOnInit(): void {
+    this.candidatosService.getImcPorFaixa().subscribe((dados) => {
+      const imcData = dados.imcMedioPorFaixaEtaria;
+      const categorias = Object.keys(imcData); // ["20-29", "30-39", ...]
+      const valores = Object.values(imcData);  // [24.75, 25.5, ...]
+
+      this.chartOptionsImc = {
+        series: [
+          {
+            name: 'Média IMC',
+            data: valores,
+          },
+        ],
+        chart: {
+          type: 'bar',
+          height: 350,
+        },
+        title: {
+          text: 'IMC por Faixa Etária',
+        },
+        xaxis: {
+          categories: categorias,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+          },
+        },
+      };
+    });
+  }
 }
