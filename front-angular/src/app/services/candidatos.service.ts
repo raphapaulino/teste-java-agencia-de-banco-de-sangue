3c5ce29d3d8199563584f59084440990b700ba9d
Raphael Paulino <raphael.paulino.web@gmail.com>
2025-06-12 04:49:50 -0300

    feat: cria candidatos.service e configurações de ambientes prod e dev separadas

---
+++ front-angular/src/app/services/candidatos.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CandidatosService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getImcPorFaixa(): Observable<{ imcMedioPorFaixaEtaria: { [faixa: string]: number } }> {
+    return this.http.get<{ imcMedioPorFaixaEtaria: { [faixa: string]: number } }>(
+      `${this.apiUrl}/api/candidatos/imc-por-candidato`
+    );
+  }
+}
